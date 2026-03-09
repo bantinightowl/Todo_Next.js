8201e6f3d8c10080a66c9e114c3d2250fb8191df
bantinightowl <bantikumar@digitalnightowl.in>
2026-03-09 16:33:03 +0530

    re-back the same code

--- middleware.js
+++ middleware.js
@@ -1,61 +1,30 @@
-// // middleware.js
-// import { auth } from "@/lib/auth";
-// import { NextResponse } from "next/server";
-
-// const publicPaths = ['/login', '/register'];
-
-// export default auth((req) => {
-//   const { pathname } = req.nextUrl;
-//   const isLoggedIn = !!req.auth;
-//   const isPublicPath = publicPaths.some(path => pathname.startsWith(path));
-
-//   // If not logged in and not on public pages, redirect to login
-//   if (!isLoggedIn && !isPublicPath) {
-//     const url = req.nextUrl.clone();
-//     url.pathname = '/login';
-//     return NextResponse.redirect(url);
-//   }
-
-//   // If logged in and on login/register pages, redirect to home
-//   if (isLoggedIn && isPublicPath) {
-//     const url = req.nextUrl.clone();
-//     url.pathname = '/';
-//     return NextResponse.redirect(url);
-//   }
-
-//   return NextResponse.next();
-// });
-
-// export const config = {
-//   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-// };
-
-
-
+// middleware.js
+import { auth } from "@/lib/auth";
 import { NextResponse } from "next/server";
 
 const publicPaths = ['/login', '/register'];
 
-export function middleware(req) {
+export default auth((req) => {
   const { pathname } = req.nextUrl;
-  const token = req.cookies.get('next-auth.session-token')?.value;
-
+  const isLoggedIn = !!req.auth;
   const isPublicPath = publicPaths.some(path => pathname.startsWith(path));
 
-  if (!token && !isPublicPath) {
+  // If not logged in and not on public pages, redirect to login
+  if (!isLoggedIn && !isPublicPath) {
     const url = req.nextUrl.clone();
     url.pathname = '/login';
     return NextResponse.redirect(url);
   }
 
-  if (token && isPublicPath) {
+  // If logged in and on login/register pages, redirect to home
+  if (isLoggedIn && isPublicPath) {
     const url = req.nextUrl.clone();
     url.pathname = '/';
     return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
-}
+});
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
